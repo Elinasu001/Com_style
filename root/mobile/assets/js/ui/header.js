ad6fa6d2e586790846c7b27d7507ddddc370bb28
suelina001 <elinaen001@gmail.com>
2025-02-10 13:31:04 +0900

    [작업중] 팝업 레이아웃 작업중중

--- root/mobile/assets/js/ui/header.js
+++ root/mobile/assets/js/ui/header.js
@@ -10,10 +10,10 @@ $(document).ready(function(){
         return {
             headerAni: function() {
                 if (scrollY > 0) {
-                    $('#headerWrap').addClass('on');
+                    $('.headerOn').addClass('on');
                     
                 } else {
-                    $('#headerWrap').removeClass('on');
+                    $('.headerOn').removeClass('on');
                 }
             },
             // 스크롤 처리
@@ -38,5 +38,45 @@ $(document).ready(function(){
     $(window).on('resize', function () {
         headerL.scrolling();
     });
+
+
+    /**
+     *  com popup
+    **/
+    var popupL = (function() {
+
+        return {
+            openPopup : function(id) {
+                var _target = $('#' + id);
+                _target.fadeIn(200, function(){
+                    $('body').css({'overflow': 'hidden'});
+                    _target.addClass('on');
+                });
+            },
+
+            closePopup : function(id){
+                var _target = $('#' + id);
+                _target.fadeOut(200, function() {
+                    _target.removeClass('on');
+                    if ($('.layerPopup.on').length === 0) {
+                        $('body').css({ 'overflow': '' });
+                    }
+                });
+            },
+            togglePopup: function(id) {
+                var _target = $('#' + id);
+                if (_target.hasClass('on')) {
+                    this.closePopup(id);
+                } else {
+                    this.openPopup(id);
+                }
+            }
+        }
+    })();
+
+    // 전역
+    window.openPopup = popupL.openPopup;
+    window.closePopup = popupL.closePopup;
+    window.togglePopup = popupL.togglePopup;
 
 });
