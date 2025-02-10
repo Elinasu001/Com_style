b9a1ac582227153780fac01721601743965cb483
suelina001 <elinaen001@gmail.com>
2025-02-10 14:47:01 +0900

    [완료료] 팝업

--- root/mobile/assets/js/ui/header.js
+++ root/mobile/assets/js/ui/header.js
@@ -39,44 +39,4 @@ $(document).ready(function(){
         headerL.scrolling();
     });
 
-
-    /**
-     *  com popup
-    **/
-    var popupL = (function() {
-
-        return {
-            openPopup : function(id) {
-                var _target = $('#' + id);
-                _target.fadeIn(200, function(){
-                    $('body').css({'overflow': 'hidden'});
-                    _target.addClass('on');
-                });
-            },
-
-            closePopup : function(id){
-                var _target = $('#' + id);
-                _target.fadeOut(200, function() {
-                    _target.removeClass('on');
-                    if ($('.layerPopup.on').length === 0) {
-                        $('body').css({ 'overflow': '' });
-                    }
-                });
-            },
-            togglePopup: function(id) {
-                var _target = $('#' + id);
-                if (_target.hasClass('on')) {
-                    this.closePopup(id);
-                } else {
-                    this.openPopup(id);
-                }
-            }
-        }
-    })();
-
-    // 전역
-    window.openPopup = popupL.openPopup;
-    window.closePopup = popupL.closePopup;
-    window.togglePopup = popupL.togglePopup;
-
 });
