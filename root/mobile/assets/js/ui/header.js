085ca805ea03471418f2daca0e7ea109ba37aec2
suelina001 <elinaen001@gmail.com>
2025-02-10 15:31:46 +0900

    [팝업 _  정리] 공통 부분 정리리

--- root/mobile/assets/js/ui/header.js
+++ root/mobile/assets/js/ui/header.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    /*---------------------------------------------
+        ready, load, resize
+    ---------------------------------------------*/
+    $(document).ready(function(){
+        headerPopupL.scrolling();
+    });
+
+    // $(window).on('load',function(){
+    // });
+
+    $(window).on('resize', function () {
+        headerL.scrolling();
+        headerPopupL.scrolling();
+    });
+    
 
     /**
      * header on클래스 추가
@@ -10,10 +25,10 @@ $(document).ready(function(){
         return {
             headerAni: function() {
                 if (scrollY > 0) {
-                    $('.headerOn').addClass('on');
+                    $('.headerOn:visible').addClass('on');
                     
                 } else {
-                    $('.headerOn').removeClass('on');
+                    $('.headerOn:visible').removeClass('on');
                 }
             },
             // 스크롤 처리
@@ -34,9 +49,33 @@ $(document).ready(function(){
     $(window).on('scroll', function() {
         headerL.scrolling();
     });
-    //resize
-    $(window).on('resize', function () {
-        headerL.scrolling();
+
+    /**
+     * 팝업 header on 클래스 추가
+     **/
+    var headerPopupL = (function() {
+
+        var scrollY = 0; // 스크롤 위치 저장
+
+        return {
+            headerAni: function() {
+                var headerOn = $('.fullPopupOn:visible .pop-header');
+                if (scrollY > 0) {
+                    headerOn.addClass('on');
+                } else {
+                    headerOn.removeClass('on');
+                }
+            },
+            scrolling: function () {
+                scrollY = $('.fullPopupOn .pop-con-wrap').scrollTop();
+                this.headerAni();
+            }
+        };
+    })();
+
+    // event
+    $('.fullPopupOn .pop-con-wrap').on('scroll', function() {
+        headerPopupL.scrolling();
     });
 
 });
