62b052d28005916419db6b9cfe5dd5804a07cbf4
suelina001 <elinaen001@gmail.com>
2025-02-11 10:56:54 +0900

    ㅛㅜ정정

--- root/mobile/assets/js/ui/popup.js
+++ root/mobile/assets/js/ui/popup.js
@@ -9,16 +9,17 @@ $(document).ready(function(){
             openPopup : function(id) {
                 var _target = $('#' + id);
                 _target.addClass('on');
+                _target.find('.pop-con-wrap').scrollTop(0);// 스크롤 초기화
                 $('body').css({'overflow': 'hidden'});
-                $('.pop-con-wrap').scrollTop(0);
-            },
 
+            },
             closePopup : function(id){
                 var _target = $('#' + id);
                 _target.removeClass('on');
-                if ($('.layerPopup.on').length === 0) {//마지막 팝업
+                if ($('.layerPopup.on').length === 0) {// 마지막 열린 팝업
                     $('body').css({ 'overflow': '' });
                 }
+                
             },
             togglePopup: function(id) {
                 var _target = $('#' + id);
@@ -30,9 +31,10 @@ $(document).ready(function(){
             },
             closePopupOnDim: function(){
                 $('.layerPopup.btmSheet').on('click', function(e){
-                    if(!$(e.target).closest('.pop-con-wrap').length){
+                    if(e.target === this){
                         popupL.closePopup($(this).attr('id'));
                     }
+                    e.stopPropagation();
                 });
             }
         }
