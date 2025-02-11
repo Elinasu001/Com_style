30a8dbf7524feb25d4453e9d3575e1f2ca947934
sub-suelina001 <elinaen001@gmail.com>
2025-02-11 21:51:38 +0900

    수정중중

---
+++ root/mobile/assets/js/ui/com/input.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    /**
+     * 3자리 수마다 콤마 적용 기능
+    **/
+    var commaFormatter = (function() {
+        
+        return {
+            format: function(input) {
+                var value = input.value.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거
+                value = value.replace(/,/g, ''); // 기존 ',' 제거
+                value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // 3자리마다 ',' 추가
+                input.value = value;
+            }
+        };
+    })();
+
+    // 이벤트
+    $(document).on('keyup', 'input[inputmode=numeric]', function() {
+        commaFormatter.format(this);
+    });
+});
--- root/mobile/assets/js/ui/ui-util.js
+++ root/mobile/assets/js/ui/ui-util.js
@@ -1,6 +1,8 @@
 /*
 ** 공통
 */
+import "./com/input.js";
 import "./com/layout.js";
 import "./com/popup.js";
+import "./com/terms.js";
 
