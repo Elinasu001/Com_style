d175fe1a8df976bfa8ac3cfe7acf44b618008e5b
suelina001 <elinaen001@gmail.com>
2025-02-13 10:02:17 +0900

    js 팝업 분리

--- root/mobile/assets/js/ui/com/popup.js
+++ root/mobile/assets/js/ui/com/popup.js
@@ -1,32 +1,59 @@
 $(document).ready(function(){
 
     /**
-     *  com popup
+     *  공통 팝업
+     *  openPopup :: full 팝업, btmSheet 팝업
+     *  openModal :: modal
+     *  openMenu :: 전체 메뉴
     **/
     var popupL = (function() {
 
+        // 헬퍼 함수
+        function initPopup(id) {
+            var _target = $('#' + id);
+            _target.addClass('on');
+            _target.find('.pop-con-wrap').scrollTop(0);
+            $('body').css({'overflow': 'hidden'});
+            return _target;
+        }
+
         return {
             openPopup : function(id) {
-                var _target = $('#' + id);
-                _target.addClass('on');
-                _target.find('.pop-con-wrap').scrollTop(0);// 스크롤 초기화
-                $('body').css({'overflow': 'hidden'});
-
+                var _target = initPopup(id);
+            },
+            openModal: function(id, msg) {
+                var _target = initPopup(id);
+                if(msg) {
+                    _target.find('.modal').html(msg);
+                }
+            },
+            openMenu: function(id, menu){
+                var _target = initPopup(id);
+                if(menu){
+                    _target.find('.allMenu').html(menu);
+                }
             },
             closePopup : function(id){
                 var _target = $('#' + id);
                 _target.removeClass('on');
-                if ($('.layerPopup.on').length === 0) {// 마지막 열린 팝업
+                if ($('.layerPopup.on').length === 0) { // 마지막 열린 팝업
                     $('body').css({ 'overflow': '' });
                 }
                 
             },
-            togglePopup: function(id) {
+            togglePopup: function(id, msg, menu) {
                 var _target = $('#' + id);
                 if (_target.hasClass('on')) {
                     this.closePopup(id);
                 } else {
-                    this.openPopup(id);
+                    if(msg){
+                        this.openModal(id, msg);
+                    }else if(menu){
+                        this.openMenu(id, menu);
+                    }else{
+                        this.openPopup(id);
+                    }
+                    
                 }
             },
             closePopupOnDim: function(){
@@ -42,6 +69,8 @@ $(document).ready(function(){
 
     // 전역
     window.openPopup = popupL.openPopup;
+    window.openModal = popupL.openModal;
+    window.openMenu = popupL.openMenu;
     window.closePopup = popupL.closePopup;
     window.togglePopup = popupL.togglePopup;
 
